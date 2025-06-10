831eaf043e76c4cb25e8101cd02c6cbc6bd8044b
hippocampusb <tweichin09@gmail.com>
2025-06-10 13:16:45 +0800

    mamamia

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 import src.utils.ETL as etl
 
 extracted_df = etl.extract_csv("/Users/tweic/code/YEE-TEE-EL/data/bank.csv")
-print(extracted_df.head())
+print(extracted_df.dtypes)
--- src/utils/ETL.py
+++ src/utils/ETL.py
@@ -5,3 +5,5 @@ import pandas as pd
 def extract_csv(file_path):
     df = pd.read_csv(file_path, sep=";")
     return df
+
+# create transform functions
