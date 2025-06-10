9ff3ff65e29da5f79b51febf8972e58477816a49
hippocampusb <tweichin09@gmail.com>
2025-06-10 15:58:56 +0800

    new data

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 
 import src.utils.ETL as etl
 
-extracted_df = etl.extract_csv("/Users/tweic/code/YEE-TEE-EL/data/bank.csv")
-print(extracted_df.dtypes)
--- src/utils/ETL.py
+++ src/utils/ETL.py
@@ -3,7 +3,18 @@ import pandas as pd
 
 # create extract functions
 def extract_csv(file_path):
-    df = pd.read_csv(file_path, sep=";")
+    df = pd.read_csv(file_path, encoding="latin1")
+
+    # checks for data quality
+    print(df.info)
+    # check duplicates
+
     return df
 
 # create transform functions
+
+
+# EDA for data quality checks
+sales_df = extract_csv("/Users/tweic/code/YEE-TEE-EL/data/Large Cars Dataset.csv")
+
+# address line 2, state, postalcode,and territory have null values address line is negligible look into the others
